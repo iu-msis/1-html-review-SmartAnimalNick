153666d004543649ad9fe9e85ec065d5d2714b4f
SmartAnimalNick <nick@smartanimaltraining.com>
2021-09-15 15:29:37 -0400

    Change from Method 1 to Method 2
    Method 1 as discussed in class was the preferred method by Professor Gregory. Method 2 (now commented out) was working with a button, but is not the preferred method.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,29 +1,54 @@
-const app = Vue.createApp({
+// const app = Vue.createApp({
+//     data() {
+//         return {
+//             picture: 'https://randomuser.me/api/portraits/men/75.jpg',
+//             firstName: 'John',
+//             lastName: 'Doe',
+//             country: 'United States',
+//             dateDob: '2000-04-28T20:07:37.751Z',
+//             ageDob: 21,
+//             email: 'john@gmail.com',
+//         }
+//     },
+    //  methods: {
+    //      async getUser() {
+    //          const res = await fetch('https://randomuser.me/api')
+    //          const { results } = await res.json()
+
+//             this.picture = results[0].picture.large
+//             this.firstName = results[0].name.first
+//             this.lastName = results[0].name.last
+//             this.country = results[0].location.country
+//             this.dateDob = results[0].dob.date
+//             this.ageDob = results[0].dob.age
+//             this.email = results[0].email
+//         },
+//     },
+// })
+
+// app.mount('#app')
+
+
+const SomeApp = {
     data() {
-        return {
-            picture: 'https://randomuser.me/api/portraits/men/75.jpg',
-            firstName: 'John',
-            lastName: 'Doe',
-            country: 'United States',
-            dateDob: '2000-04-28T20:07:37.751Z',
-            ageDob: 21,
-            email: 'john@gmail.com',
-        }
+      return {
+        result: {},
+        list: [5,6,7,8],
+        message: "Waiting ...",
+      }
     },
-    methods: {
-        async getUser() {
-            const res = await fetch('https://randomuser.me/api')
-            const { results } = await res.json()
+    created() {
 
-            this.picture = results[0].picture.large
-            this.firstName = results[0].name.first
-            this.lastName = results[0].name.last
-            this.country = results[0].location.country
-            this.dateDob = results[0].dob.date
-            this.ageDob = results[0].dob.age
-            this.email = results[0].email
-        },
-    },
-})
-
-app.mount('#app')
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((json) => {
+            console.log("Got json back:", json);
+            this.result = json.results[0];
+        })
+        .catch( (error) => {
+            console.error(error);
+        });
+    }
+  }
+  
+  Vue.createApp(SomeApp).mount('#someApp');
