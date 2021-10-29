26143ecc0b89b789022decd843e3b8312823dc10
SmartAnimalNick <nick@smartanimaltraining.com>
2021-10-29 15:37:45 -0400

    Edit/Delete

--- app/public/js/table.js
+++ app/public/js/table.js
@@ -10,6 +10,7 @@ const SomeApp = {
             pgcount: {},
             msrp: {},
         },
+        selectedBook: null,
         "bookForm": {}
       }
     },
@@ -39,12 +40,74 @@ const SomeApp = {
             })
             .then( response => response.json() )
             .then( json => {
-                console.log("Returned from post", json);
+                console.log("Returned from post:", json);
                 this.books = json;
-                this.bookForm={};
+                this.resetBookForm();
+            })
+            .catch( err => {
+              alert("Something went horribly wrong!");
             });
-        }
+        },
+
+        postEditBook(evt) {     
+            
+            console.log("Updating!", this.bookForm);
+
+            fetch('api/books/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+            .then( response => response.json() )
+            .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                this.resetBookForm();
+            });
+        },
+        postDeleteBook(o) {
+            if (!confirm("Are you sure you want to delete the book titled "+o.booktitle+"?")) {
+                return;
+            }
+            
+            fetch('api/books/delete.php', {
+                method:'POST',
+                body: JSON.stringify(o),
+                headers: {
+                "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+            .then( response => response.json() )
+            .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                this.resetBookForm();
+            });
+        },
+        selectBook(o) {
+            this.selectedBook = o;
+            this.bookForm = Object.assign({}, this.selectedBook);
+        },
+
+        resetBookForm() {
+            this.selectedBook = null;
+            this.bookForm = {};
+        },
+        postBook(evt) {
+            if (this.selectedBook === null) {
+                this.postNewBook(evt);
+            } else {
+                this.postEditBook(evt);
+            }
+          }
     },
+
     created() {
         this.fetchBooksData();
     }
